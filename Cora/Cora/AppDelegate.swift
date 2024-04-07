444f5b6387b6b4085de2f7be54e87073828daf30
5nilha <bwtdirect@gmail.com>
2024-04-06 22:43:38 -0300

    Applying UI Theme

--- Cora/Cora/AppDelegate.swift
+++ Cora/Cora/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoraUI
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        ThemeManager.applyTheme(theme: .light)
+        setupNavigationControllerUI()
         return true
     }
 
@@ -31,6 +34,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
+    private func setupNavigationControllerUI() {
+        UINavigationBar.appearance().backgroundColor = ThemeManager.current.lightColor
+        UINavigationBar.appearance().tintColor = ThemeManager.current.primaryColor
+    }
 }
-
