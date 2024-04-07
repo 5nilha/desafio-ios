91db7d0a9d940ad165413a2dbbcf5a1139a92c98
5nilha <bwtdirect@gmail.com>
2024-04-07 09:04:24 -0300

    Create BaseView.swift

---
+++ CoraUI/Sources/CoraUI/UIComponents/BaseView.swift
@@ -0,0 +1,36 @@
+//
+//  File.swift
+//  
+//
+//  Created by Fabio Quintanilha on 07/04/24.
+//
+
+import UIKit
+
+public class BaseView: UIView {
+
+    // MARK: - Initialization
+    /// Initializes the BaseView with a frame.
+    /// - Parameter frame: The initial frame for the base view.
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    /// Initializes the BaseView from data in a given unarchiver (coder).
+    /// - Parameter coder: An unarchiver object.
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+
+    /// Initializes the BaseView with a zero frame (convenience initializer).
+    public convenience init() {
+        self.init(frame: .zero)
+    }
+
+    /// Common initialization method called from various initializers.
+    private func commonInit() {
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
