6afcc178da50b6d78b3ec1c47160810f9b740ddb
5nilha <bwtdirect@gmail.com>
2024-04-07 01:38:13 -0300

    Creating HomeViewController

---
+++ Cora/Cora/Views/ViewControllers/Home/HomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewController.swift
+//  Cora
+//
+//  Created by Fabio Quintanilha on 07/04/24.
+//
+
+import Foundation
+
+class HomeViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
