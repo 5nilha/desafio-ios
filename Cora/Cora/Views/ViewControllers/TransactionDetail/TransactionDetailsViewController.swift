90819576c0fa0ba68b6d1888930490950388587a
5nilha <bwtdirect@gmail.com>
2024-04-07 01:38:39 -0300

    Create TransactionDetailsViewController.swift

---
+++ Cora/Cora/Views/ViewControllers/TransactionDetail/TransactionDetailsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TransactionDetailsViewController.swift
+//  Cora
+//
+//  Created by Fabio Quintanilha on 07/04/24.
+//
+
+import UIKit
+
+class TransactionDetailsViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
