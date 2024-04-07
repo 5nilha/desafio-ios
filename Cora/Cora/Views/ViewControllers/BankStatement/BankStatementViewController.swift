59e636410f01effa4b795ab2468f4214615a7244
5nilha <bwtdirect@gmail.com>
2024-04-07 01:38:34 -0300

    Create BankStatementViewController.swift

---
+++ Cora/Cora/Views/ViewControllers/BankStatement/BankStatementViewController.swift
@@ -0,0 +1,15 @@
+//
+//  BankStatementViewController.swift
+//  Cora
+//
+//  Created by Fabio Quintanilha on 07/04/24.
+//
+
+import UIKit
+
+class BankStatementViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
