52cb8597d46e804488740a297950668c1b37aecd
5nilha <bwtdirect@gmail.com>
2024-04-07 01:38:23 -0300

    Create LoginProfileViewController.swift

---
+++ Cora/Cora/Views/ViewControllers/Login/LoginProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginProfileViewController.swift
+//  Cora
+//
+//  Created by Fabio Quintanilha on 07/04/24.
+//
+
+import UIKit
+
+class LoginProfileViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
