ec801ffcce1198a2003a360af101ee1061cb6ec0
5nilha <bwtdirect@gmail.com>
2024-04-07 01:38:25 -0300

    Create LoginPasswordViewController.swift

---
+++ Cora/Cora/Views/ViewControllers/Login/LoginPasswordViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginPasswordViewController.swift
+//  Cora
+//
+//  Created by Fabio Quintanilha on 07/04/24.
+//
+
+import UIKit
+
+class LoginPasswordViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
