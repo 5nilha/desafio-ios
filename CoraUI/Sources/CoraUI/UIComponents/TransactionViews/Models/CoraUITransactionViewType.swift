2d4ca09460380187a9e04e189c31a1065ccf9b77
5nilha <bwtdirect@gmail.com>
2024-04-09 17:31:35 -0300

    Create CoraUITransactionViewType.swift

---
+++ CoraUI/Sources/CoraUI/UIComponents/TransactionViews/Models/CoraUITransactionViewType.swift
@@ -0,0 +1,14 @@
+//
+//  CoraUITransactionViewType.swift
+//  
+//
+//  Created by Fabio Quintanilha on 09/04/24.
+//
+
+import Foundation
+
+public enum CoraUITransactionViewType {
+    case income
+    case outcome
+    case returned
+}
