74b2c925d6d137c62c3cf0ad465cf65557da0bba
5nilha <bwtdirect@gmail.com>
2024-04-09 17:33:09 -0300

    Adding ImageAsset resources

---
+++ Cora/Cora/Utils/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Cora
+//
+//  Created by Fabio Quintanilha on 09/04/24.
+//
+
+import Foundation
+
+struct ImageAssets {
+    static var eyes: String  = "ic_eye-hidden"
+    static var returnTransaction: String = "ic_arrow-return"
+    static var incomeTransaction: String = "ic_arrow-down-in"
+    static var outcomeTransaction: String = "ic_arrow-up-out"
+}
