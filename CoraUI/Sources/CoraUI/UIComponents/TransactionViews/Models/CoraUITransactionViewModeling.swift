befe3607d673376f6c9accadcb869ee9d0da77d9
5nilha <bwtdirect@gmail.com>
2024-04-09 17:31:28 -0300

    Create CoraUITransactionViewModeling.swift

---
+++ CoraUI/Sources/CoraUI/UIComponents/TransactionViews/Models/CoraUITransactionViewModeling.swift
@@ -0,0 +1,17 @@
+//
+//  CoraUITransactionViewModeling.swift
+//  
+//
+//  Created by Fabio Quintanilha on 09/04/24.
+//
+
+import Foundation
+
+public protocol CoraUITransactionViewModeling {
+    var imageName: String { get }
+    var amount: String { get }
+    var description: String { get }
+    var sender: String { get }
+    var time: String { get }
+    var transactionType: CoraUITransactionViewType { get }
+}
