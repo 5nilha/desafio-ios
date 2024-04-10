996468df9ad176fba21b49681ebb5bb5f809bfb1
5nilha <bwtdirect@gmail.com>
2024-04-10 10:43:04 -0300

    Update SceneDelegate.swift

--- Cora/Cora/SceneDelegate.swift
+++ Cora/Cora/SceneDelegate.swift
@@ -6,11 +6,13 @@
 //
 
 import UIKit
+import CoraUI
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
     var appCoordinator: AppCoordinator?
+    private let navigationController = UINavigationController()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
@@ -18,11 +20,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = navigationController
 
-        let navigationViewController = UINavigationController()
-        window?.rootViewController = navigationViewController
-
-        appCoordinator = AppCoordinator(navigationController: navigationViewController)
+        appCoordinator = AppCoordinator(navigationController: navigationController)
         appCoordinator?.start()
 
         window?.makeKeyAndVisible()
@@ -55,7 +55,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
 
