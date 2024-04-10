f5e444644b4eb168749d69dd16033954c4f59ff8
5nilha <bwtdirect@gmail.com>
2024-04-10 10:40:33 -0300

    Update Theme.swift

--- CoraUI/Sources/CoraUI/Themes/Theme.swift
+++ CoraUI/Sources/CoraUI/Themes/Theme.swift
@@ -20,7 +20,7 @@ public enum UITheme: Theme {
     case dark
 
     public var primaryColor: UIColor {
-        return UIColor(red: 1.00, green: 0.24, blue: 0.43, alpha: 1.00)
+        return UIColor(hex: "#FE3E6D")
     }
 
     public var secondaryColor: UIColor {
@@ -32,23 +32,23 @@ public enum UITheme: Theme {
     }
 
     public var accentColor: UIColor {
-        return UIColor(red: 0.10, green: 0.58, blue: 0.85, alpha: 1.00)
+        return UIColor(hex: "#1A93DA")
     }
 
     public var darkTextColor: UIColor {
-        return UIColor(red: 0.23, green: 0.23, blue: 0.23, alpha: 1.00)
+        return UIColor(hex: "#3B3B3B")
     }
 
     public var grayTextColor: UIColor {
-        return UIColor(red: 0.42, green: 0.44, blue: 0.46, alpha: 1.00)
+        return UIColor(hex: "#6B7076")
     }
 
     public var grayedOutColor: UIColor {
-        return UIColor(red: 0.78, green: 0.80, blue: 0.81, alpha: 1.00)
+        return UIColor(hex: "#C7CBCF")
     }
 
     public var lightColor: UIColor {
-        return UIColor(red: 0.94, green: 0.96, blue: 0.97, alpha: 1.00)
+        return UIColor(hex: "#F0F4F8")
     }
 }
 
